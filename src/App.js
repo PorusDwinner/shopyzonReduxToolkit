9827bba9028416d767c7b06934380b19b6f6acba
PorusDwinner <amit2811995@gmail.com>
2023-06-29 08:50:15 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Topbar from "./components/Topbar";
 import Home from "./page/Home";
 import Wishlist from "./page/Wishlist";
 import Cart from "./page/Cart";
+import Login from "./page/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Topbar/>
         <Routes>
+          <Route exact path="/user-login" element = {<Login/>} />
           <Route exact path="/" element = {<Home/>} />
           <Route exact path="/your-wishlist" element = {<Wishlist/>} />
           <Route exact path="/your-cart" element = {<Cart/>} />
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const Login = () => {
+
   return (
-    <div>Login</div>
+    <div className="flex justify-center items-center
+    bg-gradient-to-r from-indigo-500 from-10%
+    via-sky-500 via-30% to-emerald-500 to-90% ... h-screen">
+
+      <div className="w-[50%] shadow-2xl py-20 px-10 flex justify-center items-center ">
+
+          <div className="flex flex-col space-y-4">
+            <input className="text-slate-400 py-1 px-8"
+            type="text" placeholder="User Name..."/>
+
+            <input className="text-slate-400 py-1 px-8"
+            type="text" col={10} placeholder="Password..."/>
+
+            <button className="bg-yellow-600 text-slate-600 font-bold px-4 py-1
+            hover:bg-yellow-500">
+              Login
+            </button>
+          </div>
+
+      </div>
+
+    </div>
   )
 }
 
